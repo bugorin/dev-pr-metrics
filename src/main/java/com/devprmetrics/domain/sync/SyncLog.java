e82ccb2c69432366bf4b317c2fe225380c7811dc
Caio Bugorin <caio.bugorin@alura.com.br>
2026-03-05 22:35:46 -0300

    Add last_success and last_failure fields to SyncLog

--- src/main/java/com/devprmetrics/domain/sync/SyncLog.java
+++ src/main/java/com/devprmetrics/domain/sync/SyncLog.java
@@ -2,12 +2,12 @@ package com.devprmetrics.domain.sync;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
 @Table(name = "SyncLog")
 public class SyncLog {
 
@@ -19,22 +19,44 @@ public class SyncLog {
     @Column(name = "type", nullable = false, length = 100)
     private SyncLogOption type;
 
+    @Getter
     @Column(name = "last_sync")
     private LocalDateTime lastSync;
+
+    @Column(name = "last_success")
+    private LocalDateTime lastSuccess;
+
+    @Column(name = "last_failure")
+    private LocalDateTime lastFailure;
 
     protected SyncLog() {}
 
     public SyncLog(SyncLogOption type) {
         this.type = type;
         this.lastSync = LocalDate.now().minusDays(type.getStartDaysAgo()).atStartOfDay();
+        this.lastSuccess = LocalDate.now().minusDays(type.getStartDaysAgo()).atStartOfDay();
     }
 
     public LocalDateTime nextTimeToTry() {
-        LocalDateTime next = this.lastSync.plusHours(this.type.getTimeToTryHours());
+        LocalDateTime next = this.lastSuccess.plusHours(this.type.getTimeToTryHours());
         LocalDateTime now = LocalDateTime.now();
         if (next.isAfter(now)) {
             return now.minusSeconds(this.type.getDelaySeconds());
         }
         return next;
+    }
+
+    public SyncLog success(LocalDateTime lastSync) {
+        if(lastSync == null) throw new IllegalArgumentException("lastSync cannot be null");
+        this.lastSync = lastSync;
+        this.lastSuccess = LocalDateTime.now();
+        return this;
+    }
+
+    public SyncLog error() {
+        if(lastSync == null) throw new IllegalArgumentException("lastSync cannot be null");
+        this.lastSync = LocalDateTime.now();
+        this.lastFailure = LocalDateTime.now();
+        return this;
     }
 }
